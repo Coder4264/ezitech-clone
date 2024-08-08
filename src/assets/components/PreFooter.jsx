f6f606de378b53b92949bf6a54442eaf195db1bc
Coder4264 <91794687+Coder4264@users.noreply.github.com>
2024-08-08 17:07:19 +0500

    Update PreFooter.jsx

--- src/assets/components/PreFooter.jsx
+++ src/assets/components/PreFooter.jsx
@@ -14,8 +14,8 @@ export default function PreFooter() {
           <h1 className='w-full text-5xl font-bold text-center md:text-left text-gray-700 py-5'>Learning <span className='text-blue-600'>anytime </span> from anywhere</h1>
           <p className='text-wrap text-justify w-full text-lg'>Increase the mobility levelwith Eziline Technology LMS App. Take your course in your pocket and access them whenever you want without any limits. Make your learning more engaging and productive using modern LMS App.</p>
           <div className='flex gap-5'>
-            <img src="https://ezitech.org/wp-content/uploads/2022/11/appstore-copy.webp" className='cursor-pointer' alt="" />
-            <img src="https://stylemixthemes.com/masterstudy/tech/wp-content/uploads/sites/21/2021/02/%D0%98%D0%B7%D0%BE%D0%B1%D1%80%D0%B0%D0%B6%D0%B5%D0%BD%D0%B8%D0%B5_22.png" alt="" />
+            <img src="https://ezitech.org/wp-content/uploads/2022/11/appstore-copy.webp" className='cursor-pointer w-auto' alt="" />
+            <img className='cursor-pointer w-auto' src="https://stylemixthemes.com/masterstudy/tech/wp-content/uploads/sites/21/2021/02/%D0%98%D0%B7%D0%BE%D0%B1%D1%80%D0%B0%D0%B6%D0%B5%D0%BD%D0%B8%D0%B5_22.png" alt="" />
           </div>
         </div>
       </div>
