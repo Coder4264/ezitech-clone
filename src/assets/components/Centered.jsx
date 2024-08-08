57f05470ae761eb3ca074bdf47375601c75c15c6
Coder4264 <91794687+Coder4264@users.noreply.github.com>
2024-08-08 17:13:40 +0500

    Update Centered.jsx

--- src/assets/components/Centered.jsx
+++ src/assets/components/Centered.jsx
@@ -8,11 +8,11 @@ export default function Centered() {
                 <h1 className='md:text-5xl text-4xl  text-white text-center font-bold w-full md:inline hidden'>Learn the fundamentals with our</h1>
                 <div className='flex mx-auto flex-col md:flex-row px-12 py-5 md:ml-28 ml-0'>
                     <h1 className=' md:text-5xl md:inline hidden text-4xl text-white text-center font-bold'>Experts in </h1>
-                    <h1 className='animation-text text-sky-300 md:text-5xl text-5xl px-2 text-center font-bold'></h1>
+                    <h1 className='animation-text text-sky-300 md:text-5xl text-2xl px-2 text-center font-bold'></h1>
                 </div>    
             </div>
             <div>
-                <h1 className='md:text-2xl text-3xl md:font-semibold font-bold text-white  md:mt-10 text-center p-5'>Utilize Effective Learning To Reach Your Potential!</h1>
+                <h1 className='text-2xl md:font-semibold font-bold text-white  md:mt-10 text-center p-5'>Utilize Effective Learning To Reach Your Potential!</h1>
             </div>
             <div className='md:flex hidden items-center mx-auto md:w-6/12  bg-white px-3 rounded-xl'>
                 <input type="search" placeholder='Search courses...' className=' w-full py-4 p-2 outline-none' />
